d326c5cd483e33830610297482b31df057699d04
ZinaBudilova <55877758+ZinaBudilova@users.noreply.github.com>
2021-10-22 12:21:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-# from ... import Searcher
+import re
+from Classic import *
+import json
 
 app = Flask(__name__)
 
@@ -10,8 +12,19 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form["query"]
-    #result = Searcher.process_query(query)
-    return render_template('index.html', query=query) #result=result
+    pattern=re.compile('^(([А-Яа-я]+|"[А-Яа-я]+"|[A-Z]{1,5})(\ |\+))*([А-Яа-я]+|"[А-Яа-я]+"|[A-Z]{1,5})$')
+    if pattern.fullmatch(query):
+        json_result = a.process_query(query)
+        if type(json_result) is dict:
+            error = "Данных по вашему запросу не найдено."
+            return render_template('error.html', error=error)
+        result = json.loads(json_result)
+        return render_template('index.html', query=query, result=result)
+    else:
+        error = "Ошибка! Неправильный формат запроса."
+        return render_template('error.html', error=error)
 
 if __name__ == '__main__':
+    with open("corpus", "rb") as f:
+        a = pickle.load(f)
     app.run(debug=False)
